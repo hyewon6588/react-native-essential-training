e7dee84f07a0b74fb2066145d71245ed085a65b5
hyewon6588 <hyewon6588@naver.com>
2024-06-17 02:40:56 +0900

    Created new project using expo, added style component sectionTitle

---
+++ HelloRN/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.sectionTitle}>Step One</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  sectionTitle:{
+    fontSize:24,
+    fontWeight:'600',
+  },
+});
