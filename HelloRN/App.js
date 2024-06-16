39c682658e8fc3a436f05e043102bfa20167c938
hyewon6588 <hyewon6588@naver.com>
2024-06-17 03:34:02 +0900

    added Text elements, explored with css - flex attribute

--- HelloRN/App.js
+++ HelloRN/App.js
@@ -4,20 +4,27 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.sectionTitle}>Step One</Text>
+      <Text style={[styles.title,{flex:5}]}>Step One</Text>
+      <Text style={[styles.title,{flex:4}]}>Step Two</Text>
+      <Text style={[styles.title,{flex:3}]}>Step Three</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop:40,
+    paddingHorizontal:24,
+    backgroundColor:'orange',
+    flex:1,
+    justifyContent:'space-around',
+    // alignItems:'center',
   },
-  sectionTitle:{
-    fontSize:24,
-    fontWeight:'600',
+  title:{
+    fontSize: 24,
+    fontWeight: '600',
+    color:'blue',
+    backgroundColor:'gray',
+    margin:10,
   },
 });
